1641fc6c2f4ea871066e246875f1313d201c597b
CodeIGuess <codeiguess@gmail.com>
2021-08-12 22:29:37 -0400

    `else` test

--- output/test.py
+++ output/test.py
@@ -2,19 +2,13 @@ _print = print
 def print(a):
     _print(str(a),end='')
     return str(a)
-class Entity: #{
-    x = 5;
-    y = 3;
-    def __init__(self, a, b): #{
-        this.x;
-        a undefined= b;
-    #}
-    def sum(self): #{
-        return (x + y);
-    #}
-#};
-e = None;
-# class `Entity`;
-e = Entity(10);
-(print(e.sum()) + print("\n"));
+if (0): #{
+    print("If!");
+#}
+elif (0): #{
+    print("Else if!");
+#}
+else: #{
+    print("Else!");
+#}
 _print();
